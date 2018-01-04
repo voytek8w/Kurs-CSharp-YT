9639667c01743617f5e56b94e82ac8f2f66ddaac
voytek8w <wojtek8w@gmail.com>
2018-01-04 21:26:36 +0100

    usunąłem args[0]

--- CSharp4/Program.cs
+++ CSharp4/Program.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSharp4
+namespace CSharp4 //to jest moja własna przestrzeń nazw
 {
-    class Program
+    class Program //Pełna nazwa tej klasy to class KursYTodc4 Program
     {
         static void Main(string[] args)
         {
+            //cw+tab+tab - snippet
+            Console.WriteLine("Hello Universe ");
+
         }
     }
 }
